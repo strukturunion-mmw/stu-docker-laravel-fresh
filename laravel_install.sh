02e7725fc2103fe023ac82860810049c43ffc98e
Manuel Weisbender <manuel.weisbender@strukturunion.de>
2021-02-25 17:32:02 +0100

    Adjusted Install Script to update New laravel ENV file so Masql connection works

--- laravel_install.sh
+++ laravel_install.sh
@@ -6,6 +6,7 @@ $DIR/service_down.sh
 
 # Set environment
 cp $DIR/.env.example $DIR/.env
+source=$DIR/.env
 
 # Re-Create MySQL Database
 rm -R $DIR/mysql
@@ -17,6 +18,16 @@ mkdir $DIR/src
 
 # Install Laravel
 docker-compose run --rm composer create-project laravel/laravel .
+
+# Update fresh Laravel ENV file
+ENVFILE=$DIR/src/.env
+sed -i 's/#APP_NAME=Laravel/APP_NAME=$APP_NAME/g' $ENVFILE
+sed -i 's/#DB_HOST=127.0.0.1/DB_HOST=mysql/g' $ENVFILE
+sed -i 's/#DB_DATABASE=laravel/DB_DATABASE=$DB_DATABASE/g' $ENVFILE
+sed -i 's/#DB_USERNAME=root/DB_USERNAME=$DB_USERNAME/g' $ENVFILE
+sed -i 's/#DB_PASSWORD=/DB_PASSWORD=$DB_PASSWORD/g' $ENVFILE
+
+
 docker-compose run --rm artisan migrate
 
 # Start Services
